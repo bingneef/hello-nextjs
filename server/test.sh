02e636098f9e12f1f9cb3296a5a208d5e7e40414
Bing Steup <bingsteup@gmail.com>
2019-05-06 21:03:26 +0200

    Fix travis

--- server/test.sh
+++ server/test.sh
@@ -1,5 +1,7 @@
-tag=$( git describe --abbrev=0 )\
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin\
-echo $( docker build -t bingneef/docker-nextjs:$tag . )\
-echo $tag\
-echo $( docker push bingneef/docker-nextjs:$tag )"
+#!/bin/bash
+
+tag=$( git describe --abbrev=0 )
+
+eval "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+eval $( docker build -t bingneef/docker-nextjs:$tag . )
+eval $( docker push bingneef/docker-nextjs:$tag )"
