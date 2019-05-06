4be7a5e2832bf3ccbbb3d1ea44dc6c0cbe83ee60
Bing Steup <bingsteup@gmail.com>
2019-05-06 21:45:48 +0200

    Fix test.sh

--- server/test.sh
+++ server/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 tag=$( git describe --abbrev=0 )
 
-eval "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo $DOCKER_PASSWORD | docker login --username bingneef --password-stdin
 eval $( docker build -t bingneef/docker-nextjs:$tag . )
-eval $( docker push bingneef/docker-nextjs:$tag )"
+eval $( docker push bingneef/docker-nextjs:$tag )
