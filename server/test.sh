9abe83042585712d2721a8e382aad14fa08b57e8
Bing Steup <bingsteup@gmail.com>
2019-05-06 22:09:46 +0200

    Update server/test.sh

--- server/test.sh
+++ server/test.sh
@@ -2,5 +2,5 @@
 tag=$( git describe --abbrev=0 )
 
 echo $DOCKER_PASSWORD | docker login --username bingneef --password-stdin
-eval $( docker build -t bingneef/docker-nextjs:$tag . )
-eval $( docker push bingneef/docker-nextjs:$tag )
+docker build -t bingneef/docker-nextjs:$tag .
+docker push bingneef/docker-nextjs:$tag
