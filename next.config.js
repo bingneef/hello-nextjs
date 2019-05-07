f190c2d805a03d0c9ab2973c8dd839b7f803e141
Bing Steup <bingsteup@gmail.com>
2019-05-07 14:39:47 +0200

    Add correct packages for next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withCSS = require("@zeit/next-css");
 const withTypescript = require("@zeit/next-typescript");
+const withSass = require('@zeit/next-sass')
 
-module.exports = withCSS({ cssMOdules: true })(withTypescript());
+module.exports = withTypescript(withSass(withCSS({ cssModules: true })));
